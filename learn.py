15dfb5f5c76aced1581260e810698b6e7c4db3bf
WangMinSSS <969821285@qq.com>
2017-06-21 12:44:23 +0800

    change to flask work

--- learn.py
+++ learn.py
@@ -1,1 +1,10 @@
-print('wangmin')
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('hello.html')
+
+if __name__ == '__main__':
+    app.run()
